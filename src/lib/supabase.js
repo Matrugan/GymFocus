fbe08944027deab3067dde014fe535a508172646
Mateus Dos Anjos <158146802+Matrugan@users.noreply.github.com>
2026-05-12 20:47:07 -0300

    online

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -4,4 +4,12 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
+if (!supabaseUrl) {
+  throw new Error("VITE_SUPABASE_URL is missing");
+}
+
+if (!supabaseAnonKey) {
+  throw new Error("VITE_SUPABASE_ANON_KEY is missing");
+}
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
